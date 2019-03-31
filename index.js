f998b46c4eb879b3840fb0d2cf66079f3cbc0630
Chris Yates <mastercontroluser@gmail.com>
2019-03-31 11:03:48 +0000

    Warning windows added
    - Improved margin of filelist names
    - Made checkbox hover events independent of entry ones
    - Added error handlers that open a warning dialog box. This only applies to a few specific fs errors. If the window loses focus it kills itself instantly.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 function createWindow() {
-let win = new BrowserWindow({ nodeIntegration: true, icon: "./bin/img/app.png", width: 1200, height: 900, frame: false, transparent: true, vibrancy: 'ultra-dark', webPreferences: { experimentalFeatures: true } })
-    win.loadFile('voyager.html')
+let win = new BrowserWindow({ title: 'Voyager', nodeIntegration: true, icon: "./bin/img/app.png", width: 1200, height: 900, frame: false, transparent: true, vibrancy: 'ultra-dark', webPreferences: { experimentalFeatures: true } })
+win.loadFile('voyager.html')
+    ipcMain.on('ntf', (evt, alertText) => {
+        let nwin = new BrowserWindow({ title: 'Error', nodeIntegration: true, icon: "./bin/img/app.png", width: 400, height: 225, frame: false, transparent: true, vibrancy: 'ultra-dark', webPreferences: { experimentalFeatures: true }, resizable: false });
+        nwin.loadFile('notify.htm');
+        ipcMain.on('gte', (evt) => {
+            evt.sender.send('erm', alertText);
+        })
+    })
 }
 app.on('ready', createWindow)
