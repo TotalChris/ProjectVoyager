7e620d8a7ebf6094c2103dec5e429dbd6f6dfcda
TotalChris <mastercontroluser@gmail.com>
2020-04-13 23:40:13 -0400

    Defined variables to hold the places of common JQuery elements. This should help reduce calls to the root $() function

--- bin/js/v2.js
+++ bin/js/v2.js
@@ -11,31 +11,41 @@ var pathString = home;
 var hindex = -1;
 var hist = [];
 
+/* BEGIN global declarations for DOM elements */
+
+JBody = $('body');
+JContext = $('#context');
+JRename = $('#renbox');
+JFileList = $('#filelist');
+
+
+/* END global declarations for DOM elements */
+
 //Theme change handler function and init
 function init(){
-    $('#menupanel').css('style', 'none')
+    $('#menupanel').css('style', 'none');
     setTheme();
     goTo((remote.process.argv[2] != null ? remote.process.argv[2] : home), 1);
 }
 function setTheme(){
-    $('body').toggleClass('dark', 'light')
+    JBody.toggleClass('dark', 'light')
     $('#themeswitch div.panelItemSubject img').attr('src', () => {
-        return '../img/' + ($('body').hasClass('dark') ? 'lgt' : 'drk') + '.png'
-    })
-    $('#themeswitch div.panelItemText').html(($('body').hasClass('dark') ? 'Light' : 'Dark') + ' Mode')
+        return '../img/' + (JBody.hasClass('dark') ? 'lgt' : 'drk') + '.png'
+    });
+    $('#themeswitch div.panelItemText').html((JBody.hasClass('dark') ? 'Light' : 'Dark') + ' Mode')
 }
 
 //Window resizing handlers
 
 win.on('closed', () => {
     win = null;
-})
+});
 
 win.on('resize', () => {
     if (win.isMaximized() === false) {
         $('#rsz_btn img').attr('src', '../img/max.png');
     }
-})
+});
 
 function resizeWin(){
     if (win.isMaximized() === false) {
@@ -49,12 +59,11 @@ function resizeWin(){
 //Context menu event handler
 
 $(document).contextmenu((evt) => {
-    closestItems = $(evt.target).closest('td.item');
-    if (closestItems.length !== 0){ //if our target is an item,
+    if ($(evt.target).closest('td.item').length !== 0) { //if our target is an item,
         iscd = false;
-        listItem = closestItems[0];
-        if(!$(listItem).hasClass('select')){  //and and the element isn't selected at the moment,
-            if($(evt.target).hasClass('itemNameText')){ //and if the text of the element was right-clicked,
+        listItem = $(evt.target).closest('td.item')[0];
+        if (!$(listItem).hasClass('select')) {  //and and the element isn't selected at the moment,
+            if ($(evt.target).hasClass('itemNameText')) { //and if the text of the element was right-clicked,
                 selectItem(listItem, 1);  //go ahead and select it, and clear out the other selected elements.
             } else { //But, if the click happened outside the item's text,
                 deselectAll();
@@ -64,10 +73,10 @@ $(document).contextmenu((evt) => {
         $('#context-table').html(siftlib.popCMenu(evt, iscd)) //since we know we have items being selected, we can now populate/draw the context menu
     }//now just position and show it
     $("#context").css('left', evt.clientX - (evt.clientX > (window.innerWidth - 150) ? 150 : 0));
-    $('#context').css('top', evt.clientY - (evt.clientY > (window.innerWidth - 210) ? 210 : 0));
-    $('#filelist').removeClass('hover-enabled');
-    $('#context').addClass('show');
-})
+    JContext.css('top', evt.clientY - (evt.clientY > (window.innerWidth - 210) ? 210 : 0));
+    JFileList.removeClass('hover-enabled');
+    JContext.addClass('show');
+});
 
 //Window blur and click event handlers
 
@@ -77,10 +86,10 @@ $(window).click((e) => resetContext(e))
 //global interaction reset (for blur and click)
 
 function resetContext(evt){
-    if($('#context').hasClass('show')){
-        $('#context').removeClass('show');
+    if(JContext.hasClass('show')){
+        JContext.removeClass('show');
         if (!($(evt.target).parent().id === 'ren')){
-            $('#filelist').addClass('hover-enabled')
+            JFileList.addClass('hover-enabled')
         };
         siftlib.clearPMenu();
     }
@@ -92,14 +101,11 @@ function deselectAll(){
     l = $('.select').length;
     if (l === 0){
         siftlib.popPMenu(true);
-        return;
     } else {
         Object.entries($('.select')).forEach((entry, i) => {
             if (i < l){
                 $(entry).removeClass('select');
                 $(entry).children('div.topbtn').children('img').attr('src', `../img/${entry[1].attributes.type.value === "file" ? 'fil' : 'fld'}.png`);
-            } else {
-                return
             }
         });
     }
@@ -145,22 +151,22 @@ function menuPanel(){
 
 function renameItem(itemName){
     deselectAll();
-    $('#filelist').removeClass('hover-enabled');
+    JFileList.removeClass('hover-enabled');
     $(`#${itemName} div.itemNameText`).html(`<input type="text" id="renbox" onkeydown="textFieldKeyHandler(event)" autofocus="autofocus"></input>`);
-    $('#renbox').val($('#renbox').closest('td.item').attr('id'));
-    $('#renbox').blur((evt) => {renameSubmit(evt)})
-    $('#renbox').keypress((evt) => {if(evt.key === 'Enter'){renameSubmit(evt)}})
+    JRename.val(JRename.closest('td.item').attr('id'));
+    JRename.blur((evt) => {renameSubmit(evt)})
+    JRename.keypress((evt) => {if(evt.key === 'Enter'){renameSubmit(evt)}})
 
 }
 
 function renameSubmit(evt){
-    if($('#renbox').closest('td.item').attr('id') !== evt.target.value){
-        fs.rename(pathString + $('#renbox').closest('td.item').attr('id'), pathString + evt.target.value, (err) => {
+    if(JRename.closest('td.item').attr('id') !== evt.target.value){
+        fs.rename(pathString + JRename.closest('td.item').attr('id'), pathString + evt.target.value, (err) => {
             if (err) throw err;
         })
     }
-    $('#renbox').parent().html(`<div class="itemNameText">${evt.target.value}</div>`)
-    $('#filelist').addClass('hover-enabled');
+    JRename.parent().html(`<div class="itemNameText">${evt.target.value}</div>`)
+    JFileList.addClass('hover-enabled');
 }
 
 //textbox key handler
@@ -201,15 +207,15 @@ function goTo(newPathString, navflag) { //NO-jquery
             ftype = (fs.statSync(newPathString).isFile() ? 'file' : 'folder');
             if (ftype === 'folder') {
                 fs.readdir(newPathString, {'encoding': 'utf8', 'withFileTypes': true}, (err, files) => {
-                    $('#filelist').html('');
+                    JFileList.html('');
                     files.forEach((element) => {
                         if (element.isDirectory()) {
-                            $('#filelist').append(`<tr><td type="folder" id="${(element.name.indexOf('.') === 0 ? '\\' : '') + element.name}" class="item"><div class="topbtn" onmouseover="if(!this.parentElement.classList.contains('select')){this.children[0].src='../img/chk0.png';};" onmouseout="if(!this.parentElement.classList.contains('select')){this.children[0].src='../img/fld.png'}"><img src="../img/fld.png" onclick="selectItem(this.parentElement.parentElement, 0)"/></div><div class="itemRow" onclick="selectItem(this.parentElement, 1)" ondblclick="goTo(pathString + '${element.name}', 1)"><div class="itemNameText">${element.name}</div></div></td></tr>`)
+                            JFileList.append(`<tr><td type="folder" id="${(element.name.indexOf('.') === 0 ? '\\' : '') + element.name}" class="item"><div class="topbtn" onmouseover="if(!this.parentElement.classList.contains('select')){this.children[0].src='../img/chk0.png';};" onmouseout="if(!this.parentElement.classList.contains('select')){this.children[0].src='../img/fld.png'}"><img src="../img/fld.png" onclick="selectItem(this.parentElement.parentElement, 0)"/></div><div class="itemRow" onclick="selectItem(this.parentElement, 1)" ondblclick="goTo(pathString + '${element.name}', 1)"><div class="itemNameText">${element.name}</div></div></td></tr>`)
                         }
                     })
                     files.forEach((element) => {
                         if (element.isFile()) {
-                            $('#filelist').append(`<tr><td type="file" id="${element.name}" class="item"><div class="topbtn" onmouseover="if(!this.parentElement.classList.contains('select')){this.children[0].src='../img/chk0.png';};" onmouseout="if(!this.parentElement.classList.contains('select')){this.children[0].src='../img/fil.png'}"><img src="../img/fil.png" onclick="selectItem(this.parentElement.parentElement, 0)"/></div><div class="itemRow" onclick="selectItem(this.parentElement, 1)" ondblclick="goTo(pathString + '${element.name}', 1)"><div class="itemNameText">${element.name}</div></div></td></tr>`)
+                            JFileList.append(`<tr><td type="file" id="${element.name}" class="item"><div class="topbtn" onmouseover="if(!this.parentElement.classList.contains('select')){this.children[0].src='../img/chk0.png';};" onmouseout="if(!this.parentElement.classList.contains('select')){this.children[0].src='../img/fil.png'}"><img src="../img/fil.png" onclick="selectItem(this.parentElement.parentElement, 0)"/></div><div class="itemRow" onclick="selectItem(this.parentElement, 1)" ondblclick="goTo(pathString + '${element.name}', 1)"><div class="itemNameText">${element.name}</div></div></td></tr>`)
                         }
                     })
                 });
